29458d87a5ba3305767141fcf4ece0f0926eb591
Boris Z <Boris Z>
2022-09-16 17:12:31 +0300

    more Compact commit

--- OrangeClass/main.swift
+++ OrangeClass/main.swift
@@ -9,24 +9,28 @@ import Foundation
 
 class Orange {
     
-    var color: String = ""
-    var taste: String = ""
-    var radius: Double
+    var color: String
+    var taste: String
+    let radius: Double
     
-    init(color: String, taste: String, radius: Double) {
+    var orangeVolume: Double {
+        calculateOrangeVolume()
+        
+    }
+    
+    init(color: String = "", taste: String = "", radius: Double) {
         self.color = color
         self.taste = taste
         self.radius = radius
     }
     
-    init(radius: Double) {
-        self.radius = radius
-    }
+//    init(radius: Double) {
+//        self.radius = radius
+//    }
+//
     
-    var orangeVolume: Double { calculateOrangeVolume(radius: radius) }
-    
-    private func calculateOrangeVolume(radius r: Double) -> Double {
-        4/3 * Double.pi * pow(3, r)
+    private func calculateOrangeVolume() -> Double {
+        4 / 3 * Double.pi * pow(radius, 3)
     }
     
 }
@@ -39,5 +43,7 @@ orange.taste = "Sweet"
 
 print("Orange has \(orange.color) color and \(orange.taste) taste. The volume of orange is \(orange.orangeVolume)")
 
+let orange2 = Orange(color: "Red", taste: "Bed", radius: 95)
 
+print("Orange has \(orange2.color) color and \(orange2.taste) taste. The volume of orange is \(orange2.orangeVolume)")
 
