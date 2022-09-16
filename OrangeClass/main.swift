735d97e1f3ce4a599146fc89e5f960fd13fd1ad9
Boris Z <Boris Z>
2022-09-16 14:52:50 +0300

    Orange Fin Commit

--- OrangeClass/main.swift
+++ OrangeClass/main.swift
@@ -2,10 +2,42 @@
 //  main.swift
 //  OrangeClass
 //
-//  Created by Boris Alexandrov on 15.09.2022.
+//  Created by Boris Zitserman
 //
 
 import Foundation
 
-print("Hello, World!")
+class Orange {
+    
+    var color: String = ""
+    var taste: String = ""
+    var radius: Double
+    
+    init(color: String, taste: String, radius: Double) {
+        self.color = color
+        self.taste = taste
+        self.radius = radius
+    }
+    
+    init(radius: Double) {
+        self.radius = radius
+    }
+    
+    var orangeVolume: Double { calculateOrangeVolume(radius: radius) }
+    
+    private func calculateOrangeVolume(radius r: Double) -> Double {
+        4/3 * Double.pi * pow(3, r)
+    }
+    
+}
+
+let orange = Orange(radius: 3.5)
+orange.color = "Orange"
+orange.taste = "Sweet"
+
+
+
+print("Orange has \(orange.color) color and \(orange.taste) taste. The volume of orange is \(orange.orangeVolume)")
+
+
 
